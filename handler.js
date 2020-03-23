451a50170a0842851084416e5a1148cfe6951e1a
Kohki SHIKATA <no-replay@example.com>
2020-03-24 02:08:35 +0900

    puppeteer implements

--- handler.js
+++ handler.js
@@ -1,5 +1,21 @@
 const puppeteer = require('puppeteer-core')
 
 module.exports = (event, response) => {
-  response.status(200).succeed(event.query.url);
+  const url = event.query.url
+  let title = ''
+  (async (url, title) => {
+    const browser = await puppeteer.launch({
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ]
+    });
+    const page = await browser.newPage();
+    await page.goto(url); // 表示したいURL
+
+    title = page.$('title')
+
+    browser.close();
+  })();
+  response.status(200).succeed(title);
 };

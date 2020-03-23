654a6e5d3df81725531668c4b51c6b8ec1753bde
Kohki SHIKATA <no-replay@example.com>
2020-03-24 02:05:13 +0900

    simple get query

--- handler.js
+++ handler.js
@@ -1,15 +1,5 @@
 const puppeteer = require('puppeteer-core')
 
 module.exports = (event, response) => {
-  // const url = event.query.url
-  const url = 'https://google.com/'
-  (async () => {
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
-    page.setViewport({width: 1024, height: 800})
-    await page.goto(url, { waitUntil: 'domcontentloaded' });
-    const title = page.$('title')
-    await browser.close()
-  })();
-  response.status(200).succeed(title);
+  response.status(200).succeed(event.query.url);
 };

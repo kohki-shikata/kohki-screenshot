745a2822465f9a9a7683db5643412c7cfbb8f521
Kohki SHIKATA <no-replay@example.com>
2020-03-24 01:58:48 +0900

    first implementation

--- handler.js
+++ handler.js
@@ -1,5 +1,14 @@
 const puppeteer = require('puppeteer-core')
 
 module.exports = (event, response) => {
-  response.status(200).succeed(event);
+  const url = event.query.url
+  (async () => {
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    page.setViewport({width: 1024, height: 800})
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    const title = page.$('title')
+    await browser.close()
+  })();
+  response.status(200).succeed(title);
 };
